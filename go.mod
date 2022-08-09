b91603bb8562a47222ec54287e899d5a09a4429c
kernelk14 <khyleisaias@gmail.com>
2022-08-09 09:42:51 +0800

    Glang: Added Files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kernelk14/glang
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+import ( 
+    "fmt"
+    "strings"
+    "strconv"
+)
+
+type GlangOps struct {
+    Data int
+    Ident string
+    Oper string
+}
+// Stack {{{
+type Stack []int
+func (s *Stack) is_empty() bool {
+    return len(*s) == 0
+}
+func (s *Stack) push(data int) {
+    *s = append(*s, data)
+}
+func (s *Stack) pop() (int, bool) {
+    if s.is_empty() {
+        return 0, false
+    } else {
+        idx := len(*s) - 1
+        elem := (*s)[idx]
+        *s = (*s)[:idx]
+        return elem, true
+    }
+}
+// }}}
+
+// The easiest way to print
+var print = fmt.Printf
+
+func lexer(program string) {
+    var program_split = strings.Split(program, " ")
+    var stack Stack
+    for i := 0; i < len(program_split); i++ {
+        var code = program_split[i]
+        switch (code) {
+            case "+":
+                print("%s  : A Plus instruction\n", code)
+                var a, aa = stack.pop()
+                var b, ab = stack.pop()
+                stack.push(a + b)
+                print("Glang Debug [1st integer is in the stack]: ", aa)
+                print("\nGlang Debug [2nd integer is in the stack]: ", ab)
+                print("\n")
+                break
+            case "write":
+                print("%s  : A Write instruction\n", code)
+                a, aa := stack.pop()
+                if aa == true {
+                    print("Glang Debug [Result]: %d\n", a)
+                }
+                break
+            default:
+                print("%s  : Integers to be pushed\n", code)
+                c_psh, err := strconv.Atoi(code)
+                stack.push(c_psh)
+                print("Glang Debug [Stack Atoi() __err__ trace]: ", err)
+                print("\n")
+                break
+        }
+        // print("%s\n", code)
+    }
+}
+
+func main() {
+    var program = "34 35 + write"
+    lexer(program) 
+}
