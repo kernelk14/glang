955460e403781f9971060d418c9bf7c3fdeb9731
kernelk14 <khyleisaias@gmail.com>
2022-08-10 08:08:59 +0800

    Glang: Added Command Line Arguments
    I used the manual assigning of arguments

--- main.go
+++ main.go
@@ -1,114 +1,133 @@
 package main
-import ( 
-    "fmt"
-    "strings"
-    "strconv"
-    "os"
-    "regexp"
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type GlangOps struct {
-    Data int
-    Ident string
-    Oper string
+	Data  int
+	Ident string
+	Oper  string
 }
 
 // Stack {{{
 type Stack []int
+
 func (s *Stack) is_empty() bool {
-    return len(*s) == 0
+	return len(*s) == 0
 }
+
 func (s *Stack) push(data int) {
-    *s = append(*s, data)
+	*s = append(*s, data)
 }
+
 func (s *Stack) pop() (int, bool) {
-    if s.is_empty() {
-        return 0, false
-    } else {
-        idx := len(*s) - 1
-        elem := (*s)[idx]
-        *s = (*s)[:idx]
-        return elem, true
-    }
+	if s.is_empty() {
+		return 0, false
+	} else {
+		idx := len(*s) - 1
+		elem := (*s)[idx]
+		*s = (*s)[:idx]
+		return elem, true
+	}
 }
+
 // }}}
 
 // The easiest way to print
 var print = fmt.Printf
 
 func evaluate(program string) {
-    var program_split = strings.Split(program, " ")
-    var stack Stack
-    for i := 0; i < len(program_split); i++ {
-        var code = program_split[i]
-        switch (code) {
-            case "+":
-                print("%s  : A Plus instruction\n", code)
-                var a, aa = stack.pop()
-                var b, ab = stack.pop()
-                stack.push(a + b)
-                print("Glang Debug [1st integer is in the stack]: ", aa)
-                print("\nGlang Debug [2nd integer is in the stack]: ", ab)
-                print("\n")
-            case "-":
-                print("%s  : A Minus instruction\n", code)
-                var a, aa = stack.pop()
-                var b, ab = stack.pop()
-                stack.push(b - a)
-                print("Glang Debug [1st integer is in the stack]: ", aa)
-                print("\nGlang Debug [2nd integer is in the stack]: ", ab)
-                print("\n")
-            case "*":
-                print("%s  : A Multiply Instruction\n", code)
-                var a, aa = stack.pop()
-                var b, ab = stack.pop()
-                stack.push(a * b)
-                print("Glang Debug [1st integer is in the stack]: ", aa)
-                print("\nGlang Debug [2nd integer is in the stack]: ", ab)
-                print("\n")
-            case "write":
-                print("%s  : A Write instruction\n", code)
-                a, aa := stack.pop()
-                if aa {
-                    print("Glang Debug [Result]: %d\n", a)
-                }
-            case "write\n":     // I hardcoded this instruction with newline, because i dont have much knowledge in slicing newlines in Go.
-                print("%s  : A Write instruction\n", code)
-                a, aa := stack.pop()
-                if aa {
-                    print("Glang Debug [Result]: %d\n", a)
-                }
-            default:
-                print("%s  : Integers to be pushed\n", code)
-                c_psh, err := strconv.Atoi(code)
-                stack.push(c_psh)
-                print("Glang Debug [Stack Atoi() __err__ trace]: ", err)
-                print("\n")
-        }
-        print("----------------------------------------------\n")
-        // print("%s\n", code)
-    }
+	program_split := strings.Split(program, " ")
+	var stack Stack
+	for i := 0; i < len(program_split); i++ {
+		code := program_split[i]
+		// TODO: Comment out this Debug Messages if ever.
+		switch code {
+		case "+":
+			print("%s  : A Plus instruction\n", code)
+			a, _ := stack.pop()
+			b, _ := stack.pop()
+			stack.push(a + b)
+			// print("Glang Debug [1st integer is in the stack]: ", aa)
+			// print("\nGlang Debug [2nd integer is in the stack]: ", ab)
+			// print("\n")
+		case "-":
+			print("%s  : A Minus instruction\n", code)
+			a, _ := stack.pop()
+			b, _ := stack.pop()
+			stack.push(b - a)
+			// print("Glang Debug [1st integer is in the stack]: ", aa)
+			// print("\nGlang Debug [2nd integer is in the stack]: ", ab)
+			// print("\n")
+		case "*":
+			print("%s  : A Multiply Instruction\n", code)
+			a, _ := stack.pop()
+			b, _ := stack.pop()
+			stack.push(a * b)
+			// print("Glang Debug [1st integer is in the stack]: ", aa)
+			// print("\nGlang Debug [2nd integer is in the stack]: ", ab)
+			// print("\n")
+		case "write":
+			print("%s  : A Write instruction\n", code)
+			a, _ := stack.pop()
+			/* if aa {
+			    print("Glang Debug [Result]: %d\n", a)
+			} */
+			print("Glang Debug [Result]: %d\n", a)
+		case "write\n": // I hardcoded this instruction with newline, because i dont have much knowledge in slicing newlines in Go.
+			print("%s  : A Write instruction\n", code)
+			a, _ := stack.pop()
+			/* if aa {
+			    print("Glang Debug [Result]: %d\n", a)
+			} */
+			print("Glang Debug [Result]: %d\n", a)
+		default:
+			print("%s  : Integers to be pushed\n", code)
+			c_psh, _ := strconv.Atoi(code)
+			stack.push(c_psh)
+			// print("Glang Debug [Stack Atoi() __err__ trace]: ", err)
+			// print("\n")
+		}
+		print("----------------------------------------------\n")
+		// print("%s\n", code)
+	}
 }
 
 func check_err(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+		// print("Ur Mom\n")
+		os.Exit(1)
+	}
 }
-
-
-
 
 // strings.ReplaceAll(string(program_file), "\n", " ")
 func TrimNewLines(s string) string {
-    var re = regexp.MustCompile(` +\r?\n +`)
-    return re.ReplaceAllString(s, " ")
+	re := regexp.MustCompile(` +\r?\n +`)
+	return re.ReplaceAllString(s, " ")
+}
+
+func usage() {
+	print("Glang Programming Language\n")
+	print("Usage: glang <filename>\n")
 }
 
 func main() {
-    program_file, err := os.ReadFile("./hello2.glg")
-    check_err(err)
-    // var program = string(program_file)
-    var program = strings.TrimSuffix(string(program_file), "\n")
-    evaluate(TrimNewLines(string(program))) 
+	if len(os.Args) < 2 {
+		usage()
+		print("ERROR: Did not supply enough arguments. Maybe you forgot the filename?\n")
+		os.Exit(1)
+		// panic("Did not supply enough arguments.")
+	} else {
+		program_file, err := os.ReadFile(os.Args[1])
+		check_err(err)
+		// var program = string(program_file)
+		program := strings.TrimSuffix(string(program_file), "\n")
+		evaluate(TrimNewLines(string(program)))
+	}
 }
