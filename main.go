ab24370af176b284a94c535926cdcfc0c5d1ede1
kernelk14 <khyleisaias@gmail.com>
2022-08-10 12:43:20 +0800

    Glang: Added Warnings and `/` instruction.

--- main.go
+++ main.go
@@ -13,11 +13,19 @@ import (
 	"github.com/fatih/color"
 )
 
+// Version number
 const VERSION = "0.0.3-beta"
+
+// Stage: it's either `devel`, or `release`
+const STAGE = "devel"
 
 // Special Functions
 var red = color.New(color.FgRed).SprintFunc()
-var redC = red("ERROR:")
+
+var (
+	yellow = color.New(color.FgYellow).SprintFunc()
+	redC   = red("ERROR:")
+)
 
 func err(err_str string) {
 	print("%s %s", redC, err_str)
@@ -28,6 +36,20 @@ type GlangOps struct {
 	Ident string
 	Oper  string
 }
+
+// Here are the list of operations, after you add an operation here, go to function simulate()
+// and you will find an `if` statement, you will increment the number in the `if` statement.
+const (
+	OP_PUSH  = iota
+	OP_WRITE = iota
+	OP_PLUS  = iota
+	OP_MINUS = iota
+	OP_MULTI = iota
+	OP_DIV   = iota
+	OP_POS   = iota
+	OP_GOTO  = iota
+	OP_COUNT = iota
+)
 
 // Stack {{{
 type Stack []int
@@ -63,6 +85,10 @@ func evaluate(program string) {
 	for i := 0; i < len(program_split); {
 		code := program_split[i]
 		// TODO: Comment out this Debug Messages if ever.
+		if strings.HasPrefix(code, "\"") == true {
+			err("Strings are not implemented yet.\n")
+			break
+		}
 		switch code {
 		case "+":
 			print("%s  : A Plus instruction\n", code)
@@ -88,6 +114,11 @@ func evaluate(program string) {
 			// print("Glang Debug [1st integer is in the stack]: ", aa)
 			// print("\nGlang Debug [2nd integer is in the stack]: ", ab)
 			// print("\n")
+		case "/":
+			print("%s  : A Divide Instruction\n", code)
+			a, _ := stack.pop()
+			b, _ := stack.pop()
+			stack.push(a / b)
 		case "write":
 			print("%s  : A Write instruction\n", code)
 			a, _ := stack.pop()
@@ -146,10 +177,19 @@ func simulate(file string) {
 	check_err(err)
 	// var program = string(program_file)
 	program := strings.TrimSuffix(string(program_file), "\n")
-	evaluate(TrimNewLines(string(program)))
+
+	// OP_COUNT should be incremented.
+	if OP_COUNT != 8 {
+		print("%s: Operations not handled properly.\n", red("ERROR"))
+	} else {
+		evaluate(TrimNewLines(string(program)))
+	}
 }
 
 func main() {
+	if STAGE == "devel" {
+		print("%s: Glang is in the development stage, be careful.\n\n", yellow("WARNING"))
+	}
 	if len(os.Args) < 2 {
 		usage()
 
